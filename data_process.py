0a7ae04e9632ce07290b9010decc2d5151203082
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2019-04-20 13:28:08 -0400

    Update data_process.py
    made processing less specific to my system

--- data_process.py
+++ data_process.py
@@ -11,7 +11,7 @@ then pickles them for easy later use
 # file_type = "" is equal to your unconverted file type. why? line 19
 fy = np.array([])
 fx = np.array([])
-base = "E:/PycharmProjects/untitled/data/"
+base = "chair"  # enter the name of directory of 3D files that is filled with a single type of object
 intbase = 890
 endbase = ".binvox"
 dirfiles = os.listdir(base)
@@ -23,13 +23,13 @@ for x in dirfiles:
         if cou > 50: # I had to implement a timer because the variable would be vulnerable to bit overflow
             break # I am not being pretentious I got a memory error
         cou = cou + 1
-        path = base + x
+        path = base + "/" + x
         xtrain = read_as_3d_array(open(path, "rb"))
         xtrain = np.array(xtrain.data.astype(int))
         print(xtrain)
         fx = np.append(fx, xtrain)
-        if "chair" in x:
-            fy = np.append(fy, 0) # chair label will be later decoded via dir from 0
+        if base in x:
+            fy = np.append(fy, 0) # label will be later decoded via dir from 0
     """
     uncomment if you used binvox to convert files
      but have non converted files
@@ -38,5 +38,5 @@ for x in dirfiles:
         """
     
 print(fx)
-pickle.dump(fx, open("XChair.pickle", "wb"), protocol=pickle.DEFAULT_PROTOCOL)
-pickle.dump(fy, open("YChair.pickle", "wb"), protocol=pickle.DEFAULT_PROTOCOL)
+pickle.dump(fx, open("X" + base + ".pickle", "wb"), protocol=pickle.DEFAULT_PROTOCOL)
+pickle.dump(fy, open("Y" + base + ".pickle", "wb"), protocol=pickle.DEFAULT_PROTOCOL)
